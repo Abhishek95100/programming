5c913d4b167a5982860f74d027146584acfa43f5
Abhishek kumar verma <abhishekverma95100@gmail.com>
2025-09-26 19:41:46 +0530

    arithmetic

---
+++ arithmetic.py
@@ -0,0 +1,8 @@
+a = int(input("Enter the number:"))
+b = int(input("Enter the number:"))
+
+print("add:",a+b)
+print("subtract:",a-b)
+print("multiply:",a*b)
+print("divide:",a/b)
+print("moudulus:",a%b)
