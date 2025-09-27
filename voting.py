fae33ae8381fe1d95ff2b3728ecde9fabe67b99e
Abhishek kumar verma <abhishekverma95100@gmail.com>
2025-09-27 16:39:25 +0530

    voting

---
+++ voting.py
@@ -0,0 +1,16 @@
+# User से age input लो:
+
+# age < 18 → "Not Eligible for Voting"
+
+# age ≥ 18 → "Eligible for Voting"
+
+user = int(input ("Enter the age:"))
+
+if user< 18 :
+    print("not eligible for voting")
+else:
+# elif user > 18 :
+    print("eligible for voting")
+
+# else:
+#     print("okey")
