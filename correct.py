a412e1b92a07ad27193ff7b316007ad72ae97aba
Abhishek kumar verma <abhishekverma95100@gmail.com>
2025-09-28 17:20:28 +0530

    correct password

---
+++ correct.py
@@ -0,0 +1,13 @@
+correct_password = "Abhishek@3332"
+
+password  = input ("Enter the passowrd :")
+
+while password != correct_password:
+    
+    print ("try Again")
+
+    password = input("Enter the number")
+
+    print("Access Granted ✅")
+
+
