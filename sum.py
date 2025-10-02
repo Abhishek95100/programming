64453efc2dc606c491bd7d4a8064ade5a6736f3c
Abhishek kumar verma <abhishekverma95100@gmail.com>
2025-10-02 23:19:29 +0530

    slove

---
+++ sum.py
@@ -0,0 +1,11 @@
+def add_number(a,b): #parameter (a,b)
+    result = a+b
+    print("the sum number is:",result)
+
+#call function 
+
+add_number(2145,41222)  #arguments(2145,41222)
+
+add_number(5458,85646)
+add_number(8545,9965)
+add_number(48482,755)
